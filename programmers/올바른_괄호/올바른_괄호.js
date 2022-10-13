f576d4ebc6c8b405a905be647d1f262e676e139b
kpzzy <sdjjm95@naver.com>
2022-10-13 23:11:38 +0900

    올바른 괄호 success

--- programmers/올바른_괄호/올바른_괄호.js
+++ programmers/올바른_괄호/올바른_괄호.js
@@ -1,12 +1,15 @@
-const s = '()()';
+const s = '(()))()';
 function solution(s) {
   let answer = true;
   let num = 0;
-  let first = s[0];
-  let last = s[s.length - 1];
+  const first = s[0];
+  const last = s[s.length - 1];
 
-  const firstNum = s.match(/\(/g).length;
-  const lastNum = s.match(/\)/g).length;
+  if (first !== '(' || last !== ')') answer = false;
+
+  const firstNum = s.split('(').length - 1;
+  const lastNum = s.split(')').length - 1;
+
   // if (first === '(' && last === ')') {
   //   let firstNum = s.match(/\(/g).length;
   //   let lastNum = s.match(/\)/g).length;
@@ -15,7 +18,9 @@ function solution(s) {
   //   }
   // }
 
-  if (first !== '(' || last !== ')') answer = false;
+  // 처음엔 위와같은 방법으로 풀었었는데 match 함수가 반복문 같은 개념으로 봐야하는것 같았다
+  // 그 이유는 효율성 테스트에서 번번히 깨지고 있었음 그리하여 다른 방법인 split 함수를 이용해 보았는데
+  // 스플릿 함수를 사용하였을 때 성공 하였음
 
   if (firstNum !== lastNum) answer = false;
 
