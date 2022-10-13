2b4bb88061a7ed3bfa8cff512788b54e7f229410
kpzzy <sdjjm95@naver.com>
2022-10-13 23:02:12 +0900

    올바른 괄호

--- programmers/올바른_괄호/올바른_괄호.js
+++ programmers/올바른_괄호/올바른_괄호.js
@@ -1,15 +1,28 @@
-const s = '())(()';
+const s = '()()';
 function solution(s) {
-  var answer = false;
+  let answer = true;
+  let num = 0;
   let first = s[0];
   let last = s[s.length - 1];
 
-  if (first === '(' && last === ')') {
-    let firstNum = s.match(/\(/g).length;
-    let lastNum = s.match(/\)/g).length;
-    if (firstNum === lastNum) {
-      answer = true;
-    }
+  const firstNum = s.match(/\(/g).length;
+  const lastNum = s.match(/\)/g).length;
+  // if (first === '(' && last === ')') {
+  //   let firstNum = s.match(/\(/g).length;
+  //   let lastNum = s.match(/\)/g).length;
+  //   if (firstNum === lastNum) {
+  //     answer = true;
+  //   }
+  // }
+
+  if (first !== '(' || last !== ')') answer = false;
+
+  if (firstNum !== lastNum) answer = false;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') num += 1;
+    if (s[i] === ')') num -= 1;
+    if (num < 0) answer = false;
   }
 
   return answer;
