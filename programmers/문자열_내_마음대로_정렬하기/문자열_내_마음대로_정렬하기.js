ff422c1a3e5abdbdf1a03ffe7e93458e71605c59
kpzzy <sdjjm95@naver.com>
2022-10-24 22:37:06 +0900

    문자열 내 마음대로 정렬

--- programmers/문자열_내_마음대로_정렬하기/문자열_내_마음대로_정렬하기.js
+++ programmers/문자열_내_마음대로_정렬하기/문자열_내_마음대로_정렬하기.js
@@ -1,4 +1,26 @@
+const strings = ['sun', 'bed', 'car'];
+const n = 1;
+
 function solution(strings, n) {
   var answer = [];
-  return answer;
+  strings.sort();
+  strings.sort((a, b) => {
+    if (a[n] > b[n]) return 1;
+    if (a[n] === b[n]) return 0;
+    if (a[n] < b[n]) return -1;
+  });
+
+  return strings;
 }
+
+console.log(solution(strings, n));
+
+// https://hianna.tistory.com/409 참고자료
+
+// sort 함수가 그냥 내림차순, 오름차순으로만 썼었는데 sort함수의 여러 기능을 알게 되었음
+
+// 특히 return 1, 0, -1 로 쉽게 정렬을 할 수 있어서 유용하였음
+
+// --------------------------------------------
+
+// 맨첫줄에 한번 정렬을 하여서 뒤에 문자열이 중복되더라도 가만히 있게 하였음
