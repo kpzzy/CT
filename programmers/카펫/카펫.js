028795eed12ff4d0297ce59b01887330da23da45
kpzzy <sdjjm95@naver.com>
2022-11-07 22:54:59 +0900

    카펫

---
+++ programmers/카펫/카펫.js
@@ -0,0 +1,11 @@
+const brown = 24;
+const yellow = 24;
+
+function solution(brown, yellow) {
+  var answer = [];
+  let plus = brown + yellow;
+  console.log(plus);
+  return answer;
+}
+
+console.log(solution(brown, yellow));
